e1d91f496c5165c73f19a5b022cea8c3d7e13bee
Antonin <toninrazafison@gmail.com>
2025-01-16 16:58:07 +0300

    commencement des exercicees

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word, time) {
+    if (time < 0) {
+        return "Error";
+    } else {
+        let string = '';
+        for (i = 0; i < time; i++) {
+            string += word;
+        }
+        return string
+    }
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
