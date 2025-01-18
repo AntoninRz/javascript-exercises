26d2d3c550ba61f90032204b0b4d4bd68fcf88d0
Antonin <toninrazafison@gmail.com>
2025-01-18 20:14:56 +0300

    exercice 3 fait

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -14,3 +14,11 @@ repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
+
+/*
+    Explication: 
+    On crée une fonction expression et on met comme parametre "word et time".
+    On crée une conditon:
+    Si time est inférieur à 0. La fonction affiche "Error".
+    Sinon, on crée un variable qu'on nome string avec une valeur vide ''. et on fait un boucle ou tant que i est inférieur au time, la fonction affiche dans la valeur de string l'argument du word et le repète jusqu'à ce que i = time.
+*/
